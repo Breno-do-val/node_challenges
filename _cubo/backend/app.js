6296fdc0736db8931699c32ccd754b4c3564a4a1
Breno Ribeiro do Val <bval@acme.local>
2020-11-24 14:29:52 -0300

    Adding Swagger for API documentation

--- _cubo/backend/app.js
+++ _cubo/backend/app.js
@@ -1,15 +1,20 @@
 const port = 3003 || process.env.port;
-const app = require('express')();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const router = require('./src/routes');
 const db = require('./src/config/db');
-const { json } = require('body-parser');
 
- app.use(json());
+const swaggerUI = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
+ app.use(express.json());
 
 app.use(router);
 
 app.db = db;
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
